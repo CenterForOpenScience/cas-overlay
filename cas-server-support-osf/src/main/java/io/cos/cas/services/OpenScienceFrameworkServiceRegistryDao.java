c4421daba819ce14d2f8f56d69054216fe95e170
icereval <icereval@gmail.com>
2015-05-12 23:50:03 -0400

    OpenScienceFrameworkServiceRegistryDao (OAuth Registered Services/Applications)

---
+++ cas-server-support-osf/src/main/java/io/cos/cas/services/OpenScienceFrameworkServiceRegistryDao.java
@@ -0,0 +1,199 @@
+/*
+ * Licensed to Apereo under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Apereo licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.cos.cas.services;
+
+import org.jasig.cas.services.RegisteredService;
+import org.jasig.cas.services.ReturnAllowedAttributeReleasePolicy;
+import org.jasig.cas.services.ServiceRegistryDao;
+import org.jasig.cas.support.oauth.services.OAuthRegisteredService;
+import org.jasig.cas.util.JsonSerializer;
+import org.jasig.cas.util.services.RegisteredServiceJsonSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Implementation of <code>ServiceRegistryDao</code> that reads services definition from the
+ * Open Science Framework at the Spring Application Context initialization time.
+ *
+ * @author Michael Haselton
+ * @since 4.1.0
+ */
+public class OpenScienceFrameworkServiceRegistryDao implements ServiceRegistryDao {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OpenScienceFrameworkServiceRegistryDao.class);
+
+    @NotNull
+    private MongoOperations mongoTemplate;
+
+    /**
+     * Map of service ID to registered service.
+     */
+    private Map<Long, RegisteredService> serviceMap = new ConcurrentHashMap<>();
+
+    /**
+     * The Registered service json serializer.
+     */
+    private final JsonSerializer<RegisteredService> registeredServiceJsonSerializer;
+
+    @Document(collection="oauth")
+    private class OAuth {
+        @Id
+        private BigInteger id;
+        private String name;
+        private String description;
+        private String callbackUrl;
+        private String clientId;
+        private String clientSecret;
+        private Boolean active;
+
+        public BigInteger getId() {
+            return this.id;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return this.description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getCallbackUrl() {
+            return this.callbackUrl;
+        }
+
+        public void setCallbackUrl(String callbackUrl) {
+            this.callbackUrl = callbackUrl;
+        }
+
+        public String getClientId() {
+            return this.clientId;
+        }
+
+        public void setClientId(String clientId) {
+            this.clientId = clientId;
+        }
+
+        public String getClientSecret() {
+            return this.clientSecret;
+        }
+
+        public void setClientSecret(String clientSecret) {
+            this.clientSecret = clientSecret;
+        }
+
+        public Boolean getActive() {
+            return this.active;
+        }
+
+        public void setActive(Boolean active) {
+            this.active = active;
+        }
+
+        @Override
+        public String toString() {
+            return "OAuth [id=" + this.id + ", name=" + this.name + "]";
+        }
+    }
+
+    /**
+     * Instantiates a new Json service registry dao.
+     *
+     * @param mongoTemplate the mongo template
+     * @param registeredServiceJsonSerializer the registered service json serializer
+     */
+    public OpenScienceFrameworkServiceRegistryDao(final MongoOperations mongoTemplate, final JsonSerializer<RegisteredService> registeredServiceJsonSerializer) {
+        this.mongoTemplate = mongoTemplate;
+        this.registeredServiceJsonSerializer = registeredServiceJsonSerializer;
+    }
+
+    /**
+     * Instantiates a new Json service registry dao.
+     * Sets the path to the directory where JSON service registry entries are
+     * stored. Uses the {@link RegisteredServiceJsonSerializer} by default.
+     *
+     * @param mongoTemplate the mongo template
+     */
+    public OpenScienceFrameworkServiceRegistryDao(final MongoOperations mongoTemplate) {
+        this(mongoTemplate, new RegisteredServiceJsonSerializer());
+    }
+
+    @Override
+    public final RegisteredService save(final RegisteredService service) {
+        return null;
+    }
+
+    @Override
+    public final synchronized boolean delete(final RegisteredService service) {
+        return false;
+    }
+
+    @Override
+    public final synchronized List<RegisteredService> load() {
+        List<OAuth> oAuthServices = this.mongoTemplate.find(new Query(Criteria
+                .where("active").is(true)
+        ), OAuth.class);
+
+        ReturnAllowedAttributeReleasePolicy attributeReleasePolicy = new ReturnAllowedAttributeReleasePolicy();
+        ArrayList<String> allowedAttributes = new ArrayList<>();
+        allowedAttributes.add("username");
+        allowedAttributes.add("givenName");
+        allowedAttributes.add("familyName");
+        attributeReleasePolicy.setAllowedAttributes(allowedAttributes);
+
+        final Map<Long, RegisteredService> temp = new ConcurrentHashMap<>();
+        for (final OAuth oAuthService : oAuthServices) {
+            OAuthRegisteredService service = new OAuthRegisteredService();
+            service.setId(oAuthService.getId().longValue());
+            service.setName(oAuthService.getName());
+            service.setDescription(oAuthService.getDescription());
+            service.setServiceId(oAuthService.getCallbackUrl());
+            service.setBypassApprovalPrompt(false);
+            service.setClientId(oAuthService.getClientId());
+            service.setClientSecret(oAuthService.getClientSecret());
+            service.setAttributeReleasePolicy(attributeReleasePolicy);
+            temp.put(service.getId(), service);
+        }
+        this.serviceMap = temp;
+        return new ArrayList<>(this.serviceMap.values());
+    }
+
+    @Override
+    public final RegisteredService findServiceById(final long id) {
+        return serviceMap.get(id);
+    }
+}
