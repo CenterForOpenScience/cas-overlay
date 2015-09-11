8f709f40cf21c4f44eea4559e1e0f40530a7babe
Fitz Elliott <fitz@cos.io>
2015-09-11 16:07:40 -0400

    verify successful use of MetadataPrincipalCtrl

--- cas-server-support-oauth/src/test/java/org/jasig/cas/support/oauth/web/OAuth20MetadataPrincipalControllerTests.java
+++ cas-server-support-oauth/src/test/java/org/jasig/cas/support/oauth/web/OAuth20MetadataPrincipalControllerTests.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jasig.cas.support.oauth.CentralOAuthService;
 import org.jasig.cas.support.oauth.OAuthConstants;
+import org.jasig.cas.support.oauth.metadata.PrincipalMetadata;
 import org.jasig.cas.support.oauth.token.AccessToken;
 import org.jasig.cas.support.oauth.token.InvalidTokenException;
 
@@ -17,6 +18,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * This class tests the {@link OAuth20MetadataPrincipalController} class.
@@ -31,6 +35,16 @@ public final class OAuth20MetadataPrincipalControllerTests {
     private static final String CONTENT_TYPE = "application/json";
 
     private static final String AT_ID = "AT-1";
+
+    private static final String CLIENT_ID = "client1";
+
+    private static final String PRINC_NAME_ONE = "principal_one";
+
+    private static final String PRINC_DESCR_ONE = "this is principal number one";
+
+    private static final String PRINC_NAME_TWO = "principal_two";
+
+    private static final String PRINC_DESCR_TWO = "this is principal number two";
 
     @Test
     public void verifyNoTokenOrAuthHeader() throws Exception {
@@ -137,4 +151,152 @@ public final class OAuth20MetadataPrincipalControllerTests {
         assertEquals(expectedObj.get("error_description").asText(), receivedObj.get("error_description").asText());
     }
 
+    @Test
+    public void verifyOKWithAccessToken() throws Exception {
+        final AccessToken accessToken = mock(AccessToken.class);
+        when(accessToken.getId()).thenReturn(AT_ID);
+
+        final List<PrincipalMetadata> principalMetas = Arrays.asList(
+                new PrincipalMetadata(CLIENT_ID, PRINC_NAME_ONE, PRINC_DESCR_ONE),
+                new PrincipalMetadata(CLIENT_ID, PRINC_NAME_TWO, PRINC_DESCR_TWO));
+
+        final CentralOAuthService centralOAuthService = mock(CentralOAuthService.class);
+        when(centralOAuthService.getToken(AT_ID, AccessToken.class)).thenReturn(accessToken);
+        when(centralOAuthService.getPrincipalMetadata(accessToken)).thenReturn(principalMetas);
+
+        final MockHttpServletRequest mockRequest = new MockHttpServletRequest("POST", CONTEXT
+                + OAuthConstants.METADATA_URL);
+        mockRequest.setParameter(OAuthConstants.ACCESS_TOKEN, AT_ID);
+        final MockHttpServletResponse mockResponse = new MockHttpServletResponse();
+
+        final OAuth20WrapperController oauth20WrapperController = new OAuth20WrapperController();
+        oauth20WrapperController.setCentralOAuthService(centralOAuthService);
+        oauth20WrapperController.afterPropertiesSet();
+
+        final ModelAndView modelAndView = oauth20WrapperController.handleRequest(mockRequest, mockResponse);
+        assertNull(modelAndView);
+        assertEquals(HttpStatus.SC_OK, mockResponse.getStatus());
+        assertEquals(CONTENT_TYPE, mockResponse.getContentType());
+
+        final ObjectMapper mapper = new ObjectMapper();
+
+        final String expected = "{\"data\":["
+            + "{\"client_id\":\"" + CLIENT_ID + "\",\"name\":\"" + PRINC_NAME_ONE
+            + "\",\"description\":\"" + PRINC_DESCR_ONE + "\",\"scope\":[]},"
+            + "{\"client_id\":\"" + CLIENT_ID + "\",\"name\":\"" + PRINC_NAME_TWO
+            + "\",\"description\":\"" + PRINC_DESCR_TWO + "\",\"scope\":[]}]}";
+        final JsonNode expectedObj = mapper.readTree(expected);
+        final JsonNode receivedObj = mapper.readTree(mockResponse.getContentAsString());
+
+        final JsonNode expectedData = expectedObj.get("data");
+        final JsonNode receivedData = receivedObj.get("data");
+        assertEquals(expectedData.size(), receivedData.size());
+
+        final JsonNode expectedPrincipalOne = expectedData.get(0);
+        final JsonNode receivedPrincipalOne = receivedData.get(0);
+        assertEquals(expectedPrincipalOne.get("client_id"), receivedPrincipalOne.get("client_id"));
+        assertEquals(expectedPrincipalOne.get("name"), receivedPrincipalOne.get("name"));
+        assertEquals(expectedPrincipalOne.get("description"), receivedPrincipalOne.get("description"));
+        assertEquals(expectedPrincipalOne.get("scope").size(), receivedPrincipalOne.get("scope").size());
+
+        final JsonNode expectedPrincipalTwo = expectedData.get(1);
+        final JsonNode receivedPrincipalTwo = receivedData.get(1);
+        assertEquals(expectedPrincipalTwo.get("client_id"), receivedPrincipalTwo.get("client_id"));
+        assertEquals(expectedPrincipalTwo.get("name"), receivedPrincipalTwo.get("name"));
+        assertEquals(expectedPrincipalTwo.get("description"), receivedPrincipalTwo.get("description"));
+        assertEquals(expectedPrincipalTwo.get("scope").size(), receivedPrincipalTwo.get("scope").size());
+    }
+
+    @Test
+    public void verifyOKWithAuthHeader() throws Exception {
+        final AccessToken accessToken = mock(AccessToken.class);
+        when(accessToken.getId()).thenReturn(AT_ID);
+
+        final List<PrincipalMetadata> principalMetas = Arrays.asList(
+                new PrincipalMetadata(CLIENT_ID, PRINC_NAME_ONE, PRINC_DESCR_ONE),
+                new PrincipalMetadata(CLIENT_ID, PRINC_NAME_TWO, PRINC_DESCR_TWO));
+
+        final CentralOAuthService centralOAuthService = mock(CentralOAuthService.class);
+        when(centralOAuthService.getToken(AT_ID, AccessToken.class)).thenReturn(accessToken);
+        when(centralOAuthService.getPrincipalMetadata(accessToken)).thenReturn(principalMetas);
+
+        final MockHttpServletRequest mockRequest = new MockHttpServletRequest("POST", CONTEXT
+                + OAuthConstants.METADATA_URL);
+        mockRequest.addHeader("Authorization", OAuthConstants.BEARER_TOKEN + " " + AT_ID);
+        final MockHttpServletResponse mockResponse = new MockHttpServletResponse();
+
+        final OAuth20WrapperController oauth20WrapperController = new OAuth20WrapperController();
+        oauth20WrapperController.setCentralOAuthService(centralOAuthService);
+        oauth20WrapperController.afterPropertiesSet();
+
+        final ModelAndView modelAndView = oauth20WrapperController.handleRequest(mockRequest, mockResponse);
+        assertNull(modelAndView);
+        assertEquals(HttpStatus.SC_OK, mockResponse.getStatus());
+        assertEquals(CONTENT_TYPE, mockResponse.getContentType());
+
+        final ObjectMapper mapper = new ObjectMapper();
+
+        final String expected = "{\"data\":["
+            + "{\"client_id\":\"" + CLIENT_ID + "\",\"name\":\"" + PRINC_NAME_ONE
+            + "\",\"description\":\"" + PRINC_DESCR_ONE + "\",\"scope\":[]},"
+            + "{\"client_id\":\"" + CLIENT_ID + "\",\"name\":\"" + PRINC_NAME_TWO
+            + "\",\"description\":\"" + PRINC_DESCR_TWO + "\",\"scope\":[]}]}";
+        final JsonNode expectedObj = mapper.readTree(expected);
+        final JsonNode receivedObj = mapper.readTree(mockResponse.getContentAsString());
+
+        final JsonNode expectedData = expectedObj.get("data");
+        final JsonNode receivedData = receivedObj.get("data");
+        assertEquals(expectedData.size(), receivedData.size());
+
+        final JsonNode expectedPrincipalOne = expectedData.get(0);
+        final JsonNode receivedPrincipalOne = receivedData.get(0);
+        assertEquals(expectedPrincipalOne.get("client_id"), receivedPrincipalOne.get("client_id"));
+        assertEquals(expectedPrincipalOne.get("name"), receivedPrincipalOne.get("name"));
+        assertEquals(expectedPrincipalOne.get("description"), receivedPrincipalOne.get("description"));
+        assertEquals(expectedPrincipalOne.get("scope").size(), receivedPrincipalOne.get("scope").size());
+
+        final JsonNode expectedPrincipalTwo = expectedData.get(1);
+        final JsonNode receivedPrincipalTwo = receivedData.get(1);
+        assertEquals(expectedPrincipalTwo.get("client_id"), receivedPrincipalTwo.get("client_id"));
+        assertEquals(expectedPrincipalTwo.get("name"), receivedPrincipalTwo.get("name"));
+        assertEquals(expectedPrincipalTwo.get("description"), receivedPrincipalTwo.get("description"));
+        assertEquals(expectedPrincipalTwo.get("scope").size(), receivedPrincipalTwo.get("scope").size());
+    }
+
+    @Test
+    public void verifyEmptyPrincipalListOK() throws Exception {
+        final AccessToken accessToken = mock(AccessToken.class);
+        when(accessToken.getId()).thenReturn(AT_ID);
+
+        final List<PrincipalMetadata> principalMetas = Arrays.asList();
+
+        final CentralOAuthService centralOAuthService = mock(CentralOAuthService.class);
+        when(centralOAuthService.getToken(AT_ID, AccessToken.class)).thenReturn(accessToken);
+        when(centralOAuthService.getPrincipalMetadata(accessToken)).thenReturn(principalMetas);
+
+        final MockHttpServletRequest mockRequest = new MockHttpServletRequest("POST", CONTEXT
+                + OAuthConstants.METADATA_URL);
+        mockRequest.setParameter(OAuthConstants.ACCESS_TOKEN, AT_ID);
+        final MockHttpServletResponse mockResponse = new MockHttpServletResponse();
+
+        final OAuth20WrapperController oauth20WrapperController = new OAuth20WrapperController();
+        oauth20WrapperController.setCentralOAuthService(centralOAuthService);
+        oauth20WrapperController.afterPropertiesSet();
+
+        final ModelAndView modelAndView = oauth20WrapperController.handleRequest(mockRequest, mockResponse);
+        assertNull(modelAndView);
+        assertEquals(HttpStatus.SC_OK, mockResponse.getStatus());
+        assertEquals(CONTENT_TYPE, mockResponse.getContentType());
+
+        final ObjectMapper mapper = new ObjectMapper();
+
+        final String expected = "{\"data\":[]}";
+        final JsonNode expectedObj = mapper.readTree(expected);
+        final JsonNode receivedObj = mapper.readTree(mockResponse.getContentAsString());
+
+        final JsonNode expectedData = expectedObj.get("data");
+        final JsonNode receivedData = receivedObj.get("data");
+        assertEquals(expectedData.size(), receivedData.size());
+        assertEquals(0, receivedData.size());
+    }
 }
