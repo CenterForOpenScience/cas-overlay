0361168dbc809cdc34158d1d98eb6b637d2fe523
Longze Chen <cs.longze.chen@gmail.com>
2016-03-17 12:34:52 -0400

    Separate EXPORT/IMPORT and Clean UP Code

--- cas-server-webapp/src/main/webapp/WEB-INF/crash/commands/oauth.groovy
+++ cas-server-webapp/src/main/webapp/WEB-INF/crash/commands/oauth.groovy
@@ -1,4 +1,5 @@
 import org.crsh.cli.Command
+import org.apache.commons.io.FileUtils
 import org.apache.logging.slf4j.Log4jLogger;
 import org.apache.logging.log4j.spi.ExtendedLogger
 import org.jasig.cas.authentication.principal.SimpleWebApplicationServiceImpl
@@ -37,53 +38,159 @@ class oauth {
         List<AccessTokenImpl> accessTokenList = entityManager.createQuery("select t from AccessTokenImpl t", AccessTokenImpl.class).getResultList();
         tx.commit();
 
-        StringBuilder builder = new StringBuilder();
-
-        // De/Serialization Test for TicketGrantingTicketImpl
+        //  Note: for successful import, export and import should use the same settings
+        Settings settings = new Settings();
+        settings.cleanCache();
 
         for (TicketGrantingTicketImpl ticket: ticketGrantingTicketList) {
-            System.out.println(this.JacksonSerializationTestTicketGrantingTicket(ticket));
+            jacksonSerializerTicketGrantingTicket(ticket, settings);
         }
-        builder.append(ticketGrantingTicketList.toString());
-
-        //  De/Serialization Test for ServiceTicketImpl instance
-
         for (ServiceTicketImpl ticket: serviceTicketList) {
-            System.out.println(this.JacksonSerializationTestServiceTicket(ticket));
+            jacksonSerializerServiceTicket(ticket, settings);
         }
-        builder.append(serviceTicketList.toString());
-
-        //  De/Serialization Test for RefreshTokenImpl instance
-
-        for (RefreshTokenImpl token : refreshTokenList) {
-            System.out.println(this.JacksonSerializationTestRefreshToken(token));
+        for (RefreshTokenImpl token: refreshTokenList) {
+            jacksonSerializerRefreshToken(token, settings);
         }
-        builder.append(refreshTokenList.toString());
-
-        //  De/Serialization Test for AccessTokenImpl instance
-        for(AccessTokenImpl token : accessTokenList) {
-            System.out.println(this.JacksonSerializationTestAccessToken(token));
+        for (AccessTokenImpl token: accessTokenList) {
+            jacksonSerializerAccessToken(token, settings);
         }
-        builder.append(accessTokenList.toString());
 
-        return builder.toString();
+        int totalExports = ticketGrantingTicketList.size() + serviceTicketList.size() + refreshTokenList.size() + accessTokenList.size();
+        return totalExports + " files exported";
     }
 
 
     @Command
     public String IMPORT() {
-        return "This Command Has Not Been Implemented";
+
+        Settings settings = new Settings();
+
+        List<TicketGrantingTicketImpl> ticketGrantingTicketList = new ArrayList<TicketGrantingTicketImpl>();
+        List<ServiceTicketImpl> serviceTicketList = new ArrayList<ServiceTicketImpl>();
+        List<RefreshTokenImpl> refreshTokenList = new ArrayList<RefreshTokenImpl>();
+        List<AccessTokenImpl> accessTokenList = new ArrayList<AccessTokenImpl>()
+
+        for (File file : (new File(settings.getSerializationPath())).listFiles()) {
+            if (!file.isDirectory()) {
+                if(file.getName().startsWith("TGT")) {
+                    ticketGrantingTicketList.add(jacksonDeserializerTicketGrantingTicket(file));
+                }
+                else if(file.getName().startsWith("ST")) {
+                    serviceTicketList.add(jacksonDeserializerServiceTicket(file));
+                }
+                else if(file.getName().startsWith("RT")) {
+                    refreshTokenList.add(jacksonDeserializerRefreshToken(file));
+                }
+                else if(file.getName().startsWith("AT")) {
+                    accessTokenList.add(jacksonDeserializerAccessToken(file));
+                }
+            }
+        }
+
+        //  to michael: add code here for database update
+
+        int totalImports = ticketGrantingTicketList.size() + serviceTicketList.size() + refreshTokenList.size() + accessTokenList.size();
+        return totalImports + " files imported";
+
     }
 
-
-    @Command
-    public String CLEAN() {
-        return "This Command Has Not Been Implemented";
+    /**
+     *
+     * @param file
+     * @return
+     */
+    private TicketGrantingTicketImpl jacksonDeserializerTicketGrantingTicket(File file) {
+        ObjectMapper mapper = initMapper();
+        TicketGrantingTicketImpl ticket = mapper.readValue(file, TicketGrantingTicketImpl.class);
+        return ticket;
     }
 
+    /**
+     *
+     * @param file
+     * @return
+     */
+    private ServiceTicketImpl jacksonDeserializerServiceTicket(File file) {
+        ObjectMapper mapper = initMapper();
+        ServiceTicketImpl ticket = mapper.readValue(file, ServiceTicketImpl.class);
+        return ticket;
+    }
 
-    private String JacksonSerializationTestRefreshToken(RefreshTokenImpl token) {
+    /**
+     *
+     * @param file
+     * @return
+     */
+    private RefreshTokenImpl jacksonDeserializerRefreshToken(File file) {
+        ObjectMapper mapper = initMapper();
+        RefreshTokenImpl token = mapper.readValue(file, RefreshTokenImpl.class);
+        return token;
+    }
 
+    /**
+     *
+     * @param file
+     * @return
+     */
+    private AccessTokenImpl jacksonDeserializerAccessToken(File file) {
+        ObjectMapper mapper = initMapper();
+        AccessTokenImpl token = mapper.readValue(file, AccessTokenImpl.class);
+        return token;
+    }
+
+    /**
+     *
+     * @param ticket
+     * @param settings
+     */
+    private void jacksonSerializerTicketGrantingTicket(TicketGrantingTicketImpl ticket, Settings settings) {
+        ObjectMapper mapper = initMapper();
+        File jsonFile = initFile((String)(settings.getSerializationPath() + File.separator + ticket.getId() + ".json"));
+        String jsonStr = tweakJson(mapper.writeValueAsString(ticket));
+        jsonFile.write(jsonStr);
+    }
+
+    /**
+     *
+     * @param ticket
+     * @param settings
+     */
+    private void jacksonSerializerServiceTicket(ServiceTicketImpl ticket, Settings settings) {
+        ObjectMapper mapper = initMapper();
+        File jsonFile = initFile((String)(settings.getSerializationPath() + File.separator + ticket.getId() + ".json"));
+        String jsonStr = tweakJson(mapper.writeValueAsString(ticket));
+        jsonFile.write(jsonStr);
+    }
+
+    /**
+     *
+     * @param ticket
+     * @param settings
+     */
+    private void jacksonSerializerRefreshToken(RefreshTokenImpl ticket, Settings settings) {
+        ObjectMapper mapper = initMapper();
+        File jsonFile = initFile((String)(settings.getSerializationPath() + File.separator + ticket.getId() + ".json"));
+        String jsonStr = tweakJson(mapper.writeValueAsString(ticket));
+        jsonFile.write(jsonStr);
+    }
+
+    /**
+     *
+     * @param ticket
+     * @param settings
+     */
+    private void jacksonSerializerAccessToken(AccessTokenImpl ticket, Settings settings) {
+        ObjectMapper mapper = initMapper();
+        File jsonFile = initFile((String)(settings.getSerializationPath() + File.separator + ticket.getId() + ".json"));
+        String jsonStr = tweakJson(mapper.writeValueAsString(ticket));
+        jsonFile.write(jsonStr);
+    }
+
+    /**
+     *
+     * @return
+     */
+    private ObjectMapper initMapper() {
         ObjectMapper mapper = new ObjectMapper();
 
         //  use mixin + creator to handle default constructor missing in deserialization
@@ -104,133 +211,79 @@ class oauth {
         //  enable indentation and use default pretty printer
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-        File jsonFile, jsonFileProcessed, jsonFileNew;
+        return mapper;
+    }
+
+    /**
+     *
+     * @param path
+     * @return
+     */
+    private File initFile(String path) {
+        File jsonFile;
         try {
-            jsonFile = new File("token-" + token.getId() + ".json");
-            jsonFileProcessed = new File("token-" + token.getId() + "-processed.json");
-            jsonFileNew = new File("token-" + token.getId() + "-new.json");
+            jsonFile = new File(path);
         }
-        catch (Exception e) {
-            System.out.println("Fail to open file for writing");
-            return false;
+        catch (IOException e) {
+            System.err.println("IO Exception: cannot open file");
+            throw e;
         }
+        return jsonFile;
+    }
 
-
-        String jsonStr = mapper.writeValueAsString(token);
+    /**
+     *
+     * @param jsonStr
+     * @return
+     */
+    private String tweakJson(String jsonStr) {
+        //  tweak 1:
         //  java.util.Collections$UnmodifiedMap does not have a deserializable constructor,
         //  and the above issue cannot be solved by using Jackson MixIn/Annotation,
         //  so we modify the class to its private one: java.util.HashMap.
-        String processedStr = jsonStr.replaceAll("java.util.Collections" + Matcher.quoteReplacement("\$") + "UnmodifiableMap", "java.util.HashMap");
-        jsonFileProcessed.write(processedStr);
-        RefreshTokenImpl newToken = mapper.readValue(jsonFileProcessed, RefreshTokenImpl.class);
-        mapper.writeValue(jsonFile, token);
-        mapper.writeValue(jsonFileNew, newToken);
-
-        //  will return false even all fields and super fields are equal,
-        //  not sure why, same with RefreshToken
-        return token.equals(newToken);
+        return jsonStr.replaceAll("java.util.Collections" + Matcher.quoteReplacement("\$") + "UnmodifiableMap", "java.util.HashMap");
+        //  tweak 2:
+        //  to michael: add tweak 2 here for migration
     }
 
+}
 
-    private String JacksonSerializationTestAccessToken(AccessTokenImpl token) {
 
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JacksonMixinModule());
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE);
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-        File jsonFile, jsonFileProcessed, jsonFileNew;
+public class Settings {
+
+    public Settings() {
+        File file;
         try {
-            jsonFile = new File("token-" + token.getId() + ".json");
-            jsonFileProcessed = new File("token-" + token.getId() + "-processed.json");
-            jsonFileNew = new File("token-" + token.getId() + "-new.json");
+            file = new File("./serialization-cache");
+            if (!file.exists()) {
+                file.mkdir();
+            }
         }
-        catch (Exception e) {
-            System.out.println("Fail to open file for writing");
-            return false;
+        catch (IOException e) {
+            System.err.println("IO Exception: cannot initiate working directory");
+            throw e;
         }
-
-        String jsonStr = mapper.writeValueAsString(token);
-        String processedStr = jsonStr.replaceAll("java.util.Collections" + Matcher.quoteReplacement("\$") + "UnmodifiableMap", "java.util.HashMap");
-        jsonFileProcessed.write(processedStr);
-        AccessTokenImpl newToken = mapper.readValue(jsonFileProcessed, AccessTokenImpl.class);
-        mapper.writeValue(jsonFile, token);
-        mapper.writeValue(jsonFileNew, newToken);
-
-        //  will return false even all fields and super fields are equal,
-        //  not sure why, same with RefreshToken
-        return token.equals(newToken);
+        this.serializationPath = file.getCanonicalPath();
     }
 
+    private serializationPath;
 
-
-    private String JacksonSerializationTestTicketGrantingTicket(TicketGrantingTicketImpl ticket) {
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JacksonMixinModule());
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE);
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-        File jsonFile;
-        try {
-            jsonFile = new File("ticket-" + ticket.getId() + ".json");
-            jsonFileProcessed = new File("ticket-" + ticket.getId() + "-processed.json");
-            jsonFileNew = new File("ticket-" + ticket.getId() + "-new.json");
-        }
-        catch (Exception e) {
-            System.out.println("Fail to open file for writing");
-            return false;
-        }
-
-        String jsonStr = mapper.writeValueAsString(ticket);
-        String processedStr = jsonStr.replaceAll("java.util.Collections" + Matcher.quoteReplacement("\$") + "UnmodifiableMap", "java.util.HashMap");
-        jsonFileProcessed.write(processedStr);
-        TicketGrantingTicketImpl new_ticket = mapper.readValue(jsonFileProcessed, TicketGrantingTicketImpl.class);
-        mapper.writeValue(jsonFile, ticket);
-        mapper.writeValue(jsonFileNew, newTicket);
-
-        return ticket.equals(new_ticket);
+    public getSerializationPath() {
+        return this.serializationPath;
     }
 
-
-    private String JacksonSerializationTestServiceTicket(ServiceTicketImpl ticket) {
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JacksonMixinModule());
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE);
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-        File jsonFile, jsonProcessedFile, jsonNewFile;
+    public cleanCache() {
         try {
-            jsonFile = new File("ticket-" + ticket.getId() + ".json");
-            jsonProcessedFile = new File("ticket-" + ticket.getId() + "-processed.json");
-            jsonNewFile = new File("ticket-" + ticket.getId() + "-new.json");
+            FileUtils.cleanDirectory( new File(this.serializationPath));
         }
-        catch (Exception e) {
-            System.out.println("Fail to open file for writing");
-            return false;
+        catch (IOException e) {
+            System.err.println("IO Exception: cannot clean working directory");
+            throw e;
         }
-
-        mapper.writeValue(jsonFile, ticket);
-        String jsonStr = mapper.writeValueAsString(ticket);
-        String processedStr = jsonStr.replaceAll("java.util.Collections" + Matcher.quoteReplacement("\$") + "UnmodifiableMap", "java.util.HashMap");
-        jsonProcessedFile.write(processedStr);
-
-        ServiceTicketImpl newTicket = mapper.readValue(jsonProcessedFile, ServiceTicketImpl.class);
-        mapper.writeValue(jsonNewFile, newTicket);
-
-        return ticket.equals(newTicket);
     }
 }
+
 
 
 public class JacksonMixinModule extends SimpleModule {
@@ -244,6 +297,7 @@ public class JacksonMixinModule extends SimpleModule {
         context.setMixInAnnotations(Log4jLogger.class, Log4jLoggerMixIn.class);
     }
 }
+
 
 
 abstract class SimpleWebApplicationServiceImplMixIn {
