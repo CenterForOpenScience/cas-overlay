c6e64ebe7a0e285e16466c799c7f215bb610425e
longze chen <cs.longze.chen@gmail.com>
2016-10-09 18:43:45 -0400

    Remove unused property and getters/setters.

--- cas-server-support-osf/src/main/java/io/cos/cas/adaptors/mongodb/OpenScienceFrameworkInstitutionAuthenticationHandler.java
+++ cas-server-support-osf/src/main/java/io/cos/cas/adaptors/mongodb/OpenScienceFrameworkInstitutionAuthenticationHandler.java
@@ -54,12 +54,6 @@ public class OpenScienceFrameworkInstitutionAuthenticationHandler {
         @Field("title")
         private String name;
 
-        @Field("description")
-        private String description;
-
-        @Field("institution_banner_name")
-        private String bannerName;
-
         @Field("institution_logo_name")
         private String logoName;
 
@@ -69,12 +63,6 @@ public class OpenScienceFrameworkInstitutionAuthenticationHandler {
         @Field("institution_logout_url")
         private String logoutUrl;
 
-        @Field("institution_domains")
-        private String[] domains;
-
-        @Field("institution_email_domains")
-        private String[] emailDomains;
-
         @Field("is_deleted")
         private Boolean deleted;
 
@@ -82,88 +70,24 @@ public class OpenScienceFrameworkInstitutionAuthenticationHandler {
             return nodeId;
         }
 
-        public void setNodeId(final String nodeId) {
-            this.nodeId = nodeId;
-        }
-
         public String getInstitutionId() {
             return institutionId;
-        }
-
-        public void setInstitutionId(final String institutionId) {
-            this.institutionId = institutionId;
         }
 
         public Boolean isDeleted() {
             return this.deleted;
         }
 
-        public void setDeleted(final Boolean deleted) {
-            this.deleted = deleted;
-        }
-
         public String getName() {
             return name;
-        }
-
-        public void setName(final String name) {
-            this.name = name;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(final String description) {
-            this.description = description;
-        }
-
-        public String getBannerName() {
-            return bannerName;
-        }
-
-        public void setBannerName(final String bannerName) {
-            this.bannerName = bannerName;
-        }
-
-        public String getLogoName() {
-            return logoName;
-        }
-
-        public void setLogoName(final String logoName) {
-            this.logoName = logoName;
         }
 
         public String getLoginUrl() {
             return loginUrl;
         }
 
-        public void setLoginUrl(final String loginUrl) {
-            this.loginUrl = loginUrl;
-        }
-
         public String getLogoutUrl() {
             return logoutUrl;
-        }
-
-        public void setLogoutUrl(final String logoutUrl) {
-            this.logoutUrl = logoutUrl;
-        }
-
-        public String[] getDomains() {
-            return domains;
-        }
-
-        public void setDomains(final String[] domains) {
-            this.domains = domains;
-        }
-
-        public String[] getEmailDomains() {
-            return emailDomains;
-        }
-
-        public void setEmailDomains(final String[] emailDomains) {
-            this.emailDomains = emailDomains;
         }
 
         @Override
