6edb452a57478ed32c776f4cff4fb59c10bd94fd
longze chen <cs.longze.chen@gmail.com>
2018-08-24 10:29:43 -0400

    Fix style and improve code for OSFTerminateSessionAction

--- cas-server-support-osf/src/main/java/io/cos/cas/web/flow/OpenScienceFrameworkTerminateSessionAction.java
+++ cas-server-support-osf/src/main/java/io/cos/cas/web/flow/OpenScienceFrameworkTerminateSessionAction.java
@@ -20,27 +20,30 @@
 package io.cos.cas.web.flow;
 
 import io.cos.cas.adaptors.postgres.handlers.OpenScienceFrameworkInstitutionHandler;
-import org.jasig.cas.CentralAuthenticationService;
+
+import org.apache.commons.collections4.Predicate;
+
 import org.jasig.cas.authentication.Authentication;
+import org.jasig.cas.authentication.principal.Principal;
+import org.jasig.cas.CentralAuthenticationService;
 import org.jasig.cas.ticket.Ticket;
+import org.jasig.cas.ticket.TicketGrantingTicket;
 import org.jasig.cas.web.support.CookieRetrievingCookieGenerator;
 import org.jasig.cas.web.support.WebUtils;
-import org.jasig.cas.ticket.TicketGrantingTicket;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.webflow.action.EventFactorySupport;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
+
+import java.util.Collection;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotNull;
 
-
-import org.apache.commons.collections4.Predicate;
-import org.jasig.cas.authentication.principal.Principal;
-
-import java.util.Collection;
 
 /**
  * The Open Science Framework Terminate Session Action.
@@ -124,25 +127,29 @@ public class OpenScienceFrameworkTerminateSessionAction {
      * @return Event
      */
     public Event terminate(final RequestContext context) {
-        // in login's webflow : we can get the value from context as it has already been stored
+
+        // For the login web flow : obtain the ticket ID from context as it may have already been stored
         String tgtId = WebUtils.getTicketGrantingTicketId(context);
-
-        String institutionId = null;
-        Boolean remotePrincipal = Boolean.FALSE;
-
-        // for logout, we need to get the cookie's value
+        // For the logout web flow: obtain the ticket ID from the TGC cookie
         if (tgtId == null) {
             final HttpServletRequest request = WebUtils.getHttpServletRequest(context);
             tgtId = this.ticketGrantingTicketCookieGenerator.retrieveCookieValue(request);
         }
-        // for institution logout, get the institutionId stored in TGT
+
+        // Institution login info
+        String institutionId = null;
+        Boolean remotePrincipal = Boolean.FALSE;
+
         if (tgtId != null) {
+
             TicketGrantingTicket tgt = null;
             try {
-                tgt  = centralAuthenticationService.getTicket(tgtId, TicketGrantingTicket.class);
+                tgt = centralAuthenticationService.getTicket(tgtId, TicketGrantingTicket.class);
             } catch (final Exception e) {
-                //  ignore
+                // ignore
             }
+
+            // Destroy other TGTs that belong to the same authenticated user
             if (tgt != null) {
                 final Authentication auth = tgt.getAuthentication();
                 if (auth != null) {
@@ -161,7 +168,7 @@ public class OpenScienceFrameworkTerminateSessionAction {
                             }
                         }
                     }
-                    // Obtain institution login data
+                    // Check if the session to terminate is one authenticated via institution login
                     if (auth.getAttributes().containsKey("institutionId")) {
                         institutionId = (String) auth.getAttributes().get("institutionId");
                     }
@@ -173,22 +180,28 @@ public class OpenScienceFrameworkTerminateSessionAction {
 
             WebUtils.putLogoutRequests(context, this.centralAuthenticationService.destroyTicketGrantingTicket(tgtId));
         }
+
+        // Remove cookies in the response
         final HttpServletResponse response = WebUtils.getHttpServletResponse(context);
         this.ticketGrantingTicketCookieGenerator.removeCookie(response);
         this.warnCookieGenerator.removeCookie(response);
 
-        // if logged in through institutions, redirect to institution logout endpoint
+        // Redirect to the institution logout endpoint if the session was authenticated via institution auth
         if (remotePrincipal && institutionId != null) {
             final String institutionLogoutUrl = institutionHandler.findInstitutionLogoutUrlById(institutionId);
             if (institutionLogoutUrl == null) {
-                logger.warn("Institution {} does not have a dedicated logout url, use default logout redirection instead", institutionId);
+                logger.warn(
+                        "Institution {} does not have a dedicated logout url, use default logout redirection instead",
+                        institutionId
+                );
             } else {
                 context.getFlowScope().put("logoutRedirectUrl", institutionLogoutUrl);
-                // return `finish` event to prevent `logoutRedirectUrl` being overwritten
+                // Must return the "finish" event to prevent `logoutRedirectUrl` being overwritten
                 return new Event(this, "finish");
             }
         }
 
+        // Default return event
         return this.eventFactorySupport.success(this);
     }
 }
